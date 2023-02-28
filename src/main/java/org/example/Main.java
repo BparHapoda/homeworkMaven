8aa4d665574ff9b825d4c4d9e37d9a1a886b2ced
Bpar_Hapoda <cddvdrom@yandex.ru>
2023-02-28 11:03:42 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,39 @@
-package org.example;public class Main {
+package org.example;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class Main {
+    static final String url = "jdbc:postgresql://localhost:5432/phonebook";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Connection connection = null;
+        Properties properties = new Properties();
+        properties.put("user", "postgres");
+        properties.put("password", "12345");
+
+        try {
+            connection = DriverManager.getConnection(url, properties);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM abonents");
+            while (rs.next()) {
+                System.out.println(rs.getString(2));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
